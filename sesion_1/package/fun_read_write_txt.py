dbc24a6fa070bd13b94db42436051007579b1895
paideia2000 <poencima22@gmail.com>
2025-02-26 23:54:26 +0100

    "Trabajos con ficheros y json"

--- sesion_1/package/fun_read_write_txt.py
+++ sesion_1/package/fun_read_write_txt.py
@@ -1,17 +1,19 @@
-def create_file(name_fichero: str, contenido: str)-> str:
+def overwritten_file(path_file: str, content: str)-> str:
     """ creación de fichero """
     
-    try:
-        with open(f"read_write_txt/{name_fichero}", "w", encoding="utf-8") as make:
-            return make.write(contenido)
-    except TypeError:
-        print("¡ERROR! Debe ingresar contenido válido para añadirlo al fichero.")
+    if content and not all(cont.isspace() for cont in content):
         
+        with open(path_file, "w") as f:
+            return f.write(content)
+    
+    else:
+        print("\n¡ERROR! You should add contant to the file.\n")
+
 def read_file(name_fichero: str)-> str:
     """ abrir el fichero en modo lectura """
     try:
 
-        with open(f"read_write_txt/{name_fichero}", "r") as r:
+        with open(name_fichero, "r") as r:
             return r.read()
         
     except FileNotFoundError:
@@ -32,8 +34,9 @@ def count_words(name_ficher: str)-> int:
     """ contar el numero de letras del fichero """
 
     try:
-        with open(f"read_write_txt/{name_ficher}", "r", encoding="utf-8") as counter_words:
-            number_words = list(espace for espace in counter_words.read() if espace is not " ")
+        
+        with open(f"contenido_json/{name_ficher}", "r", encoding="utf-8") as counter_words:
+            number_words = list(espace for espace in counter_words.read() if espace != " ")
             return f"\nLa secuencia sin contar los espacios tiene -» {len(number_words)} letras."
 
     except FileNotFoundError:
