8fd81510835e79a58840182213395b2e00e88a57
rene <poencima22@gmail.com>
2025-05-13 22:10:02 +0200

    added more unitaries test

--- tools/exercise/t_unitarios.py
+++ tools/exercise/t_unitarios.py
@@ -1,7 +1,6 @@
 import unittest
 
 
-
 class Test_ex_1(unittest.TestCase):
     
     def test_returned_int(self):
@@ -25,8 +24,6 @@ class Test_ex_1(unittest.TestCase):
             
             self.assertIsInstance(vl, str)
 
-
-
 class Test_Anagrama(unittest.TestCase):
 
     def test_returned_str(self):
@@ -37,6 +34,33 @@ class Test_Anagrama(unittest.TestCase):
         
         self.assertIsInstance(result,str)
         
+    def test_returned_ValueError(self):
+        """ chek if the function returner error when ani characters is not alphabetic o the funtion take a arguments empty"""
+        from anagrama import check_if_anagrama
+        
+    
+        with self.assertRaises(ValueError):
+            
+            self.assertEqual(check_if_anagrama(""), "ERROR: Please check the content of the varibales")
+            self.assertEqual(check_if_anagrama("am3or", "ERROR: The word contain other character taht is not alphabetic."))
+
+class Test_fibonacci(unittest.TestCase):
+    
+    def test_returned_int(self):
+        """ check if the function returned a interger value """
+
+        from fibonacci import suce_fibonacci
+        
+        self.assertIsInstance(suce_fibonacci(20), int)
+    
+    def test_returned_TypeError(self):
+        """ chek if the function returner typerror if not take the argument whit a number insite the variable"""
+        from fibonacci import suce_fibonacci
+        
+        with self.assertRaises(TypeError):
+            
+            self.assertEqual(suce_fibonacci(""), "ERROR: Please check the content of the varibales")
+
 
 if __name__=="__main__":
     unittest.main()
