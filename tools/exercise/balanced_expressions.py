81ec455e706462cbe0e2ca75de0d682f03b056c1
Rene <poencima22@gmail.com>
2025-05-17 15:02:21 +0200

    added balance_expression

--- tools/exercise/balanced_expressions.py
+++ tools/exercise/balanced_expressions.py
@@ -1,11 +1,39 @@
-def check_expression(expression: str, sample: dict) -> bool:
+def check_expression(expression: str) -> bool:
     """  """
-
+    import re
+    
+    miss_expresions = []
     
     if expression and isinstance(expression, str):
         
-        pass
+        new_expresion = re.findall(r'[()\[\]{}]', expression)
         
+        for e in new_expresion:
+            if e == "(":
+                if ")" not in new_expresion:
+                    miss_expresions.append(")")
+                else:
+                    continue
+                    
+            if e == "{":
+                if "}" not in new_expresion:
+                    miss_expresions.append("}")
+                else:
+                    continue
+            
+            if e == "[":
+                if "]" not in new_expresion:
+                    miss_expresions.append("]")
+                else:
+                    continue
+        
+
+        if not miss_expresions:
+            return True
+        else:
+            
+            return miss_expresions
+
     else:
         raise ValueError("ERRROR: The string must be str ando cannot be empty") 
 
@@ -18,7 +46,7 @@ def main():
     
     try:
         
-        check_expression(expression, sample)
+        print(check_expression(expression))
         
     except Exception as exp:
         print("\n", exp, "\n")
